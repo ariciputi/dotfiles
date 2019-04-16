9c6d76e43ca2117087bbf2cc0ab8d46fdf62b230
Andrea Riciputi <andrea.riciputi@gmail.com>
2019-04-17 00:11:11 +0200

    Added psql configuration

---
+++ pgsql/setup.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+BASE_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+SCRIPT_DIR="$(cd "$(dirname "$0")/../script"; pwd -P)"
+
+cd $BASE_DIR
+
+. "$SCRIPT_DIR"/common.sh
+
+create_relative_links $BASE_DIR $HOME
+
+substep_success "Done with pgsql."
+
