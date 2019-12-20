b416354958399b4c33eb84bafed1cbab3d432655
Andrea Riciputi <andrea.riciputi@gmail.com>
2019-12-20 19:14:14 +0100

    [fish] Add support for poetry completion

--- fish/setup.sh
+++ fish/setup.sh
@@ -11,6 +11,7 @@ create_dot_dir "${HOME}/.config/fish/functions"
 
 create_relative_links "${BASE_DIR}" "${HOME}/.config/fish"
 create_relative_links "${BASE_DIR}/functions" "${HOME}/.config/fish/functions"
+create_relative_links "${BASE_DIR}/completions" "${HOME}/.config/fish/completions"
 
 if [ $(which fish) ] && [ ! $(grep -q -e "fish" /etc/shells) ]; then
     substep_info "Add fish to the shells catalog"
