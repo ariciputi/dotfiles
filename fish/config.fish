b6ead995471d0b7b31d8653021f56b60b8bc2d72
Andrea Riciputi <andrea.riciputi@gmail.com>
2021-10-26 21:57:14 +0200

    [fish] Add homebrew configuration

--- fish/config.fish
+++ fish/config.fish
@@ -51,3 +51,17 @@ if command -v pyenv > /dev/null 2>&1
     status is-interactive; and pyenv init - | source
     status is-interactive; and pyenv virtualenv-init - | source
 end
+
+# Initialize brew
+if test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+
+    # Initialize brew completions
+    if test -d "$HOMEBREW_PREFIX/share/fish/completions"
+        set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/completions"
+    end
+
+    if test -d "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
+        set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
+    end
+end
