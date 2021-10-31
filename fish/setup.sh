a51414761aa11e79f1e1791816cd6094e27d1eea
Andrea Riciputi <andrea.riciputi@gmail.com>
2021-10-31 22:15:18 +0100

    Use `command -v` instead of `which`

--- fish/setup.sh
+++ fish/setup.sh
@@ -13,7 +13,7 @@ create_relative_links "${BASE_DIR}" "${HOME}/.config/fish"
 create_relative_links "${BASE_DIR}/functions" "${HOME}/.config/fish/functions"
 create_relative_links "${BASE_DIR}/completions" "${HOME}/.config/fish/completions"
 
-if [ $(which fish) ] && [ ! $(grep -q -e "fish" /etc/shells) ]; then
+if [ $(command -v fish) ] && [ ! $(grep -q -e "fish" /etc/shells) ]; then
     substep_info "Add fish to the shells catalog"
 
     sudo sh -c 'cat >> /etc/shells' <<-EOF
