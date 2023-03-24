9bd0219a61d7919471211fe0f9b16b92af3704d2
Andrea Riciputi <andrea.riciputi@gmail.com>
2023-03-24 19:47:12 +0100

    [fish] Add PG tunnel function

---
+++ fish/functions/pgt.fish
@@ -0,0 +1,76 @@
+function pgt --description 'Connect to PG using a ssh tunnel'
+
+    set --function conf_file "$HOME/.config/pgt/pgt.json"
+
+    argparse --min-args 1 --max-args 1 'verbose' -- $argv
+    or begin
+        echo "usage: pgt [--verbose] DB_LABEL"
+        return 1
+    end
+
+    if not command --query jq
+        echo "Please install jq."
+        return 1
+    else if not test -e $conf_file
+        echo "File $conf_file not found."
+        echo
+        echo 'Please create a configuration file like:
+
+DB_LABEL,connection_string
+...
+'
+        return 1
+    end
+
+    if set --query _flag_verbose
+        function _log
+            echo $argv
+        end
+    else
+        function _log
+        end
+    end
+
+    set db_label $argv[1]
+    set configuration (cat $conf_file | jq --raw-output --compact-output .)
+    set bastion_host (echo $configuration | jq --raw-output --compact-output .bastion)
+    set conn_string (echo $configuration | jq --raw-output --compact-output .dbs.$db_label)
+    set username (echo $conn_string | jq --raw-output --compact-output .username)
+    set password (echo $conn_string | jq --raw-output --compact-output .password)
+    set host (echo $conn_string | jq --raw-output --compact-output .host)
+    set port (echo $conn_string | jq --raw-output --compact-output '.port // 5432' )
+    set database (echo $conn_string | jq --raw-output --compact-output .database)
+
+    _log "Remote conn_string: postgresql://$username:$password@$host:$port/$database"
+
+    while true;
+        set local_port (random 10000 32767)
+        if not nc -z 127.0.0.1 $local_port
+            break
+        end
+    end
+
+    set tunnel_string $local_port:$host:$port $bastion_host
+    _log "SSH tunnel: $tunnel_string"
+    set local_conn_string "postgresql://$username:$password@localhost:$local_port/$database"
+    _log "Local conn_string: $local_conn_string"
+
+    echo "ssh -f -N -L $tunnel_string"
+    # TODO: replace with the actual ssh tunnel command
+    sleep 20 &
+
+    set tunnel_pid (ps xo pid,command| grep "sleep" | grep -v grep | awk '{print $1}')
+    _log "Tunnel PID: $tunnel_pid"
+
+    echo "psql $local_conn_string"
+    # TODO: replace with the actual psql command
+    vim
+
+    kill -15 $tunnel_pid
+end
+
+function __fish_print_pgt_list
+    jq --raw-output '.dbs | keys[]' "$HOME/.config/pgt/pgt.json"
+end
+
+complete --command pgt --no-files --arguments '(__fish_print_pgt_list)'
