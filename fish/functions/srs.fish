3ed979f2200d528fbe7d119260d574570fd869f0
Andrea Riciputi <andrea.riciputi@gmail.com>
2023-05-27 10:40:25 +0200

    [fish] SRS function fails gracefully if SRS is not present

--- fish/functions/srs.fish
+++ fish/functions/srs.fish
@@ -1,10 +1,15 @@
-function srs --description 'Activate SRS environment'
+function srs --description 'Activate SRS environment (if present)'
     if test -n "$argv[1]"
-        set srs_dir "$argv[1]"
+        set srs_dir "$HOME/sp/$argv[1]"
     else
-        set srs_dir srs
+        set srs_dir "$HOME/sp/srs"
     end
 
-    exec bash -c "source ~/sp/$srs_dir/activate.sh; exec fish"
+    if not test -e $srs_dir
+        echo "$srs_dir not found."
+        return 1
+    end
+
+    exec bash -c "source $srs_dir/activate.sh; exec fish"
 end
 
