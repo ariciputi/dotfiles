5fec7236bd4df50f53307639289667fa89c0f801
Andrea Riciputi <andrea.riciputi@gmail.com>
2024-02-05 22:40:14 +0100

    [fish] Fix setup script

--- fish/setup.sh
+++ fish/setup.sh
@@ -13,7 +13,7 @@ create_relative_links "${BASE_DIR}" "${HOME}/.config/fish"
 create_relative_links "${BASE_DIR}/functions" "${HOME}/.config/fish/functions"
 create_relative_links "${BASE_DIR}/completions" "${HOME}/.config/fish/completions"
 
-if [ $(command -v fish) ] && [[ "$OSTYPE" == "darwin"* ]] && [ ! $(grep -q -e "fish" /etc/shells) ]; then
+if [ $(command -v fish) ] && [ $(uname -s) = "Darwin" ] && ! $(grep -q -e "fish" /etc/shells) ; then
     substep_info "Add fish to the shells catalog"
 
     sudo sh -c 'cat >> /etc/shells' <<-EOF
